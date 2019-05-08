a5a2174e1d7e3e3d07d10ca110154dce0fa4b513
codefuldom <dominiquehallan@gmail.com>
2019-05-08 18:43:31 -0500

    :rainbow: initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-		<h1>Hi people</h1>
-		<p>Welcome to the site.</p>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-			<Image />
-		</div>
+		<h1>Learn to design and code React apps</h1>
+		<p>
+			Complete Courses about the best tools and design systems.
+			<br />
+			Prototype and build apps with React and Swift.
+		</p>
 		<Link to="/page-2/">Go to page 2</Link>
 	</Layout>
 )
