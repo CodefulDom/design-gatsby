6348da42645b013f076517d4c2bfe5e9f5ccf72c
codefuldom <dominiquehallan@gmail.com>
2019-05-08 20:27:03 -0500

    :art: working on grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import Header from "../components/header"
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+		<Header />
 		<div className="Hero">
 			<div className="HeroGroup">
 				<h1>Learn to design and code React apps</h1>
