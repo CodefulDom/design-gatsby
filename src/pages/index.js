35271fb6e5f52f5edd8d11c969cc6343156d0e3c
codefuldom <dominiquehallan@gmail.com>
2019-05-08 19:30:31 -0500

    :art: working on css

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-		<h1>Learn to design and code React apps</h1>
-		<p>
-			Complete Courses about the best tools and design systems.
-			<br />
-			Prototype and build apps with React and Swift.
-		</p>
-		<Link to="/page-2/">Watch the Video</Link>
+		<div className="Hero">
+			<div className="HeroGroup">
+				<h1>Learn to design and code React apps</h1>
+				<p>
+					Complete Courses about the best tools and design systems.
+					<br />
+					Prototype and build apps with React and Swift.
+				</p>
+				<Link to="/page-2/">Watch the Video</Link>
+			</div>
+		</div>
 	</Layout>
 )
 
